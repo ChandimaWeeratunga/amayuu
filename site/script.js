ff9594d5c5f59d8f9ec6609d5361f60f9db36a9f
Chandima Weeratunga <chandimaweeratunga97@gmail.com>
2021-01-16 00:54:03 +0530

    Modified

--- site/script.js
+++ site/script.js
@@ -27,5 +27,25 @@ const navSlide = () =>{
     
 }
 
+function readMore(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none";
+    }
+
+    else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less";
+        moreText.style.display = "inline";
+    }
+}
+
+readMore();
 navSlide();
 //Nav-bar end
+
